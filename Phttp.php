b9f2470111d137aa9ea7c7b21e2818ae6264113e
Vikram Jaswal <vikramj74@gmail.com>
2016-06-26 18:01:13 +0530

    POST method + postJson helper method

--- Phttp.php
+++ Phttp.php
@@ -3,11 +3,20 @@ class PhttpResponse {
     private $body = null;
     private $headers = null ;
     private $statusCode = null;
+    private $errorMessage= null;
     
-    public function __construct($statusCode, $body, $headers) {
+    public static function getErrorResponse($errMsg) {
+        return new PhttpResponse(null, null, null, $errMsg);
+    }
+
+    
+    public function __construct($statusCode, $body, $headers, $errMsg = null) {
         $this->statusCode =  $statusCode; 
         $this->body = $body;
         $this->headers = $headers;
+        if($errMsg !== null) {
+            $this->errorMessage = $errMsg;
+        }
     }
     
     public function getHeaders() {
@@ -22,13 +31,19 @@ class PhttpResponse {
     public function getStatusCode() {
         return $this->statusCode;
     }    
+
+    public function getErrorMessage() {
+        return $this->errorMessage;
+    }    
+    
+
 }
 
 
 
 class Phttp {
 
-    private static function parseHeaders($headerString) {
+    private static function parseResponseHeaders($headerString) {
         $headers = explode("\r\n", $headerString);
         $parsedHeaders = array();
         foreach($headers as $h) {
@@ -48,71 +63,122 @@ class Phttp {
         return $parsedHeaders;
     }
 
+
+    private static function getResponse($client) {
+        curl_setopt($client, CURLOPT_HEADER, 1); 
+        curl_setopt($client, CURLOPT_RETURNTRANSFER, 1);
+        $curlResponse = curl_exec($client);
+        $headerSize = curl_getinfo($client, CURLINFO_HEADER_SIZE);
+        $headerString = substr($curlResponse, 0, $headerSize);
+        $responseBody = substr($curlResponse, $headerSize); 
+        $responseCode = curl_getinfo($client, CURLINFO_HTTP_CODE);
+
+        $headers = Phttp::parseResponseHeaders($headerString);
+        $resp = new PhttpResponse($responseCode, $responseBody, $headers); 
+        return $resp; 
+    }
+
+    public static function attachRequestHeaders($client, $headers) {
+        $processedHeaders = array();
+        foreach($headers as $k => $v) {
+            $processedHeaders[] = $k.": ".$v;
+        }
+        echo "\nCustom request headers : ".json_encode($processedHeaders);
+        curl_setopt($client, CURLOPT_HTTPHEADER, $processedHeaders);
+    }
+
+
     public static function get($url, $args, $headers) {
+        // defaults
         if($args === null) {
             $args = array();
         }
         if($headers === null) {
             $headers = array();
         }
-        if(
-            gettype($url) !== "string" || 
-            gettype($args) !== "array" ||
-            gettype($headers) !== "array"
-        ) {
-            return null;
+
+        // argument validation
+        if(gettype($url) !== "string") {
+            return PhttpResponse::getErrorResponse("Invalid URL provided");
+        }
+        if(gettype($args) !== "array") {
+            return PhttpResponse::getErrorResponse("Invalid arguments provided");
+        }
+        if(gettype($headers) !== "array") {
+            return PhttpResponse::getErrorResponse("Invalid headers provided");
         }
        
         if(count($args) > 0) {
-            $queryString = "";
-            $argsRepr = array();
-            foreach($args as $k => $v) {
-                $repr = $k."=".$v;
-                array_push($argsRepr, $repr);
-            }
-            $queryString = join('&', $argsRepr);
+            $queryString = http_build_query($args);
             $url .= "?".$queryString;    
         }
-
-
-
-
 
         $client = curl_init();
         curl_setopt($client, CURLOPT_URL, $url);
                 
         if(count($headers) > 0) {
-            $processedHeaders = array();
-            foreach($headers as $k => $v) {
-                $processedHeaders[] = $k.": ".$v;
-            }
-            echo "\nProcessed headers : ".json_encode($processedHeaders);
-            curl_setopt($client, CURLOPT_HTTPHEADER, $processedHeaders);
+            Phttp::attachRequestHeaders($client, $headers);
         }
 
 
-        //curl_setopt($client, CURLOPT_VERBOSE, 1);
-        curl_setopt($client, CURLOPT_HEADER, 1); 
-        curl_setopt($client, CURLOPT_RETURNTRANSFER, 1);
         echo "Phttp : GETing  URL : ".$url;
 
-
-        $curlResponse = curl_exec($client);
-        $headerSize = curl_getinfo($client, CURLINFO_HEADER_SIZE);
-        $headerString = substr($curlResponse, 0, $headerSize);
-        $responseBody = substr($curlResponse, $headerSize); 
-        $responseCode = curl_getinfo($client, CURLINFO_HTTP_CODE);
+        $resp = Phttp::getResponse($client);
         curl_close($client);
-
-        $headers = Phttp::parseHeaders($headerString);
-        $resp = new PhttpResponse($responseCode, $responseBody, $headers); 
 
         return $resp;
     } 
 
-    public static function post($url, $postFields, $headers) {
-         
+    public static function post($url, $requestBody = null, $headers = null) {
+
+        if($headers === null) {
+            $headers = array();
+        } 
+        
+        // argument validation
+        if(gettype($url) !== "string") {
+            return PhttpResponse::getErrorResponse("Invalid URL provided");
+        }
+        if($requestBody !== null && gettype($requestBody) !== "string") {
+            return PhttpResponse::getErrorResponse("Invalid request body provided");
+        }
+        if(gettype($headers) !== "array") {
+            return PhttpResponse::getErrorResponse("Invalid headers provided");
+        }
+          
+        $client = curl_init();
+       
+        curl_setopt($client, CURLOPT_URL, $url); 
+
+        if($requestBody !== null) {
+           curl_setopt($client, CURLOPT_POSTFIELDS, $requestBody); 
+        }
+        
+        if(count($headers) > 0) {
+            Phttp::attachRequestHeaders($client, $headers);
+        }   
+     
+        echo "\n\nPOSTing : \n".$requestBody."\nTo : ".$url;       
+        
+        $resp = Phttp::getResponse($client);
+        curl_close($client);
+        
+        return $resp;
+          
     } 
+
+    public static function postJson($url, $jsonArr = null, $headers = null) {
+        if($jsonArr !== null && gettype($jsonArr) !== "array" ) {
+            return PhttpResponse::getErrorResponse("Invalid JSON parameter provided");
+        }
+
+        $jsonPayLoad = null;
+        if($jsonArr !== null) {
+            $jsonPayLoad = json_encode($jsonArr);   
+        }
+         
+        return Phttp::post($url, $jsonPayLoad, $headers);
+    }  
 
 
 }
